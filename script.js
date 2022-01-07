174713a5c93c20c8c5956e97ee5df069bc153603
Ayoub-Ioudiden <93585615+Ayoub-Ioudiden@users.noreply.github.com>
2022-01-07 15:36:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//function declaration
+
+function doSquareCalculation(number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+
+
+//function expression
+const doSquareCalculation = function (number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+
+
+//arrow function
+
+const doSquareCalculation = (number1, number2) => {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
